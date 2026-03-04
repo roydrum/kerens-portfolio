337135707c703b2e9dc9a0e4e1178672e6f995b3
Keren Boshi <kerenboshi@users.noreply.github.com>
2026-03-04 22:44:42 +0100

    fix: resolve React import in SectionTransition

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import { Hero } from "@/components/Hero";
 import { CaseStudies } from "@/components/CaseStudies";
 import { CreativeManagement } from "@/components/CreativeManagement";
+import { SectionTransition } from "@/components/SectionTransition";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <CaseStudies />
-      <CreativeManagement />
+      <SectionTransition>
+        <CaseStudies />
+        <CreativeManagement />
+      </SectionTransition>
     </>
   );
 }
--- components/SectionTransition.tsx
+++ components/SectionTransition.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
